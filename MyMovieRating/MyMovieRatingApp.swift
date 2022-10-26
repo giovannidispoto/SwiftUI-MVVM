76cf611a17b00e5a6b1940a7209533289787d764
Giovanni Dispoto <giovanni.dispoto@mail.polimi.it>
2022-10-26 11:24:45 +0200

    Initial Commit

---
+++ MyMovieRating/MyMovieRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieRatingApp.swift
+//  MyMovieRating
+//
+//  Created by Giovanni Dispoto on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
